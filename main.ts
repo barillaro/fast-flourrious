4ab6b674b193ab61ff11f5efcfc595764f81db6d
Sebastian Barillaro <sebastian.barillaro@gmail.com>
2025-06-24 08:01:07 +0200

    players 4->5

--- main.ts
+++ main.ts
@@ -47,7 +47,7 @@ let delyTime = defaultDelyTime;
 // print radio Message
 let printMessage = true;
 // Set the number of players!
-let players = 4;
+let players = 5;
 // Set your ID (Must be unique in your group)
 let myID = -1; // Change the random value for something fix
 // Set the difficulty of the game: "easy", (more to come later)
